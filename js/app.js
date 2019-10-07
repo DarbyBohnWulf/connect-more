ba2dcb4d1f32031cec092f3cdbfbf85d76a02adc
Mark Ritchie <mwritchie2@gmail.com>
2019-10-07 11:15:44 -0500

    improved layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
-const app = {
-    players: []
+class Player {
+    constructor(name) {
+        this.name = name;
+    }
 }
+const app = {
+    players: [],
+    nameInput: document.querySelector('#name'),
+    n
+}
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    const newPlayer = new Player(app.nameInput.value);
+    console.log(newPlayer);
+    app.players.push(newPlayer);
+    app.nameInput.setAttribute('placeholder', 'Player 2?');
+    app.nameInput.value = '';
+    
+})
