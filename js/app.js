0b4b4f656e17f3bcbd3aebc4dba522b4b5709a70
Mark Ritchie <mwritchie2@gmail.com>
2019-10-07 11:52:47 -0500

    app takes and displays names for 2 players

--- js/app.js
+++ js/app.js
@@ -6,15 +6,30 @@ class Player {
 const app = {
     players: [],
     nameInput: document.querySelector('#name'),
-    n
+    nameForm: document.querySelector('form'),
+    playArea: document.querySelector('#play-area'),
+    nameSpace: document.querySelector('#player-names'),
+    setPlayerName() {
+        const newPlayer = new Player(app.nameInput.value);
+        console.log(newPlayer);
+        this.players.push(newPlayer);
+        if (this.players.length >= 2) {
+            this.nameForm.remove();
+        } else {
+            this.nameInput.value = '';
+            this.nameInput.setAttribute('placeholder', 'Player 2?');
+        }
+        this.showPlayerName(newPlayer);
+    },
+    showPlayerName(player) {
+        const nameHeader = document.createElement('h4');
+        nameHeader.className = 'player-name';
+        nameHeader.textContent = player.name;
+        this.nameSpace.append(nameHeader);
+    }
 }
 
 document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
-    const newPlayer = new Player(app.nameInput.value);
-    console.log(newPlayer);
-    app.players.push(newPlayer);
-    app.nameInput.setAttribute('placeholder', 'Player 2?');
-    app.nameInput.value = '';
-    
+    app.setPlayerName();
 })
