d68fada1d0146ca4a209c03826d9f398b2e94e5b
Mark Ritchie <mwritchie2@gmail.com>
2019-10-08 10:23:39 -0500

    added connectionGame

---
+++ js/connectionGame.js
@@ -0,0 +1,20 @@
+/* This file is part of Connect More.
+
+Connect More is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Connect More is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Connect More.  If not, see <https://www.gnu.org/licenses/>. */
+class ConnectionGame {
+    constructor(p1,p2) {
+        this.player1 = p1;
+        this.player2 = p2;
+    }
+}
