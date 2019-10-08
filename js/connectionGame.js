abc4ac45f6275bb6f8029eab0ed97515cf26daae
Mark Ritchie <mwritchie2@gmail.com>
2019-10-08 14:44:39 -0500

    disks fall and turns continue to alternate

--- js/connectionGame.js
+++ js/connectionGame.js
@@ -25,39 +25,56 @@ class ConnectionGame {
         this.board.setAttribute('id','connect-game');
         this.board.setAttribute('width',playAreaWidth)
         this.board.setAttribute('height', playAreaWidth);
-        this.board.addEventListener('click', this.makeMove);
+        this.board.addEventListener('click', e => {
+            if (e.target.tagName === 'DIV') {
+                this.makeMove(e.target);
+            }
+        });
         console.log(this.board)
         // this.populateBoard();
         return this.board
     }
     populateBoard() {
         for (let c = 1; c <= 7; c++) {
+            const row = [];
             for (let r = 1; r <= 6; r++) {
                 const div = document.createElement('div');
                 div.setAttribute('style', `height: ${this.board.offsetWidth / 8}px;\
                 width: ${this.board.offsetWidth / 8}px; grid-column: ${c}; grid-row: ${r};\
                 background-color: #b3a024;`);
                 div.setAttribute('class','connect-slot');
-
                 div.setAttribute('id',`slot-${c}-${r}`);
                 this.board.appendChild(div);
+                row.push(div);
             }
+            this.boardMatrix.push(row);
         }
     }
-    makeMove(event) {
-        if (event.target.tagName === 'DIV') {
+    makeMove(div) {
+        if (div.style.backgroundColor !== 'rgb(179, 160, 36)') {
+            console.log('Hey, quit it!');
+        } else {
             if (app.game.turn % 2 !== 0) {
-                event.target.style.backgroundColor = '#f00f';
+                div.style.backgroundColor = '#f00f';
             } else {
-                event.target.style.backgroundColor = '#0f0f';
+                div.style.backgroundColor = '#0f0f';
             }
-            console.log(event.target);
-            // this.fall(event.target);
+            console.log(div);
+            app.game.fall(div.id);
             app.game.turn++
         }
     
     }
-    // fall(slottedDisk) {
-    //     if (slottedDisk.) 
-    // }
+    fall(slottedDiskId) {
+        // get numbers for row and column that will be equivalent in this.boardMatrix
+        const row = parseInt(slottedDiskId[7]) - 1;
+        const col = parseInt(slottedDiskId[5]) - 1;
+        console.log(row,col);
+        // look at the color of the slot below the clicked slot
+        if (row < 5 && this.boardMatrix[col][row + 1].style.backgroundColor === 'rgb(179, 160, 36)') {
+            this.boardMatrix[col][row].style.backgroundColor = 'rgb(179, 160, 36)';
+            this.makeMove(this.boardMatrix[col][row + 1])//.style.backgroundColor = this.turn % 2 !== 0 ? 'rgb(255, 0, 0)' : 'rgb(0, 255, 0)';
+            this.turn--
+        }
+    }
 }
