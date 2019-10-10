85035d62eca964bddf805c6c73e524d3c63b0700
Mark Ritchie <mwritchie2@gmail.com>
2019-10-10 11:27:24 -0500

    allow players to vote for their order

--- js/app.js
+++ js/app.js
@@ -21,8 +21,9 @@ const app = {
     mainArea: document.querySelector('main'),
     board: undefined,
     activities: [
-        'Connection Game', 'Self Destruct(!)'
+        'game-choice', 'Connection Game', 'Self Destruct(!)'
     ],
+    orderVotes: [],
     game: undefined,
     setPlayerName() {
         const newPlayer = new Player(app.nameInput.value);
@@ -30,7 +31,7 @@ const app = {
         this.players.push(newPlayer);
         // if 2 players have been named
         if (this.players.length === 2) {
-            this.transitionToChoosing();
+            this.transitionScreenTo('voting');
         // else, set page up to get P2's name
         } else {
             this.nameInput.value = '';
@@ -46,10 +47,14 @@ const app = {
         this.nameSpace.append(nameHeader);
     },
     // sets DOM up for an activity
-    transitionToChoosing() {
-        this.nameForm.remove();
-        // window.setTimeout(this.createCanvas.bind(app), 3000);
-        this.showChoices(this.activities);
+    transitionScreenTo(mainClass) {
+        this.clearArea(this.playArea);
+        this.mainArea.classList.add(mainClass);
+        if (mainClass === 'voting') {
+            this.showChoices(['player-vote', this.players[0].name, 'IDC', this.players[1].name]);
+        } else {
+            this.showChoices(this.activities);
+        }
     },
     startConnectGame() {
         this.clearArea(this.playArea);
@@ -59,38 +64,92 @@ const app = {
         this.game.populateBoard();
     },
     showChoices(choices) {
-        for (let i = 0; i < choices.length; i++) {
+        for (let i = 1; i < choices.length; i++) {
             const option = document.createElement('button');
-            option.textContent = this.activities[i];
-            option.classList.add('game-choice');
-            option.setAttribute('id',this.activities[i].split(' ').join('-').toLowerCase());
+            option.textContent = choices[i];
+            option.classList.add(choices[0]);
+            option.setAttribute('id',choices[i].split(' ').join('-').toLowerCase());
             this.playArea.appendChild(option);
+            option.addEventListener('click', this.choiceHandler)
         }
     },
-    clearArea(htmlArea) {
-        while (htmlArea.lastChild) {
-            htmlArea.removeChild(htmlArea.lastChild);
+    // can be used to empty an element or just trim from the end
+    clearArea(htmlArea, count = 0,) {
+        if (count) {
+            for (let i = 0; i < count; i++) {
+                htmlArea.removeChild(htmlArea.lastElementChild);
+            }
+        } else {
+            while (htmlArea.lastElementChild) {
+                htmlArea.removeChild(htmlArea.lastElementChild);
+            }
         }
     },
-    takeVotes() {
+    showVotes() {
         // lolololol this is gonna take a while
-        this.clearArea(this.playArea);
-        this.showChoices([this.players[0].name,this.players[1].name]);
+        
         // triggers the width transition on main
         this.mainArea.classList.add('choosing');
     },
+    takeVote(playerName) {
+        console.log('vote for', playerName);
+        this.orderVotes.push(playerName);
+        if (this.orderVotes.length < 2) {
+        } else {
+            this.clearArea(this.playArea);
+            this.tallyVotes();
+        }
+    },
+    tallyVotes() {
+        if (this.orderVotes[0] === this.orderVotes[1] && this.orderVotes[0] !== 'idc') {
+            console.log(this.orderVotes[0],'wins');
+            this.setPlayOrder(this.orderVotes[0]);
+        } else if (this.orderVotes[0] === 'idc') {
+            console.log(this.orderVotes[1],'wins');
+            this.setPlayOrder(this.orderVotes[1]);
+        } else if (this.orderVotes[1] === 'idc') {
+            console.log(this.orderVotes[0],'wins');
+            this.setPlayOrder(this.orderVotes[0]);
+        } else {
+            console.log('You don\'t care? I will choose your order.');
+            // 50/50 chance that the order will change,
+            if (Math.floor(Math.random() * 2) > 0) {
+                this.players = [this.players[1],this.players[0]]
+            }
+        }
+        this.transitionScreenTo('playing');
+    },
+    setPlayOrder(playerName) {
+        // only change order if the vote lands on player 2
+        console.log('setting order');
+        if (this.players[1].name.toLowerCase() === playerName) {
+            console.log('second player chosen');
+            this.players.unshift(this.players.splice(1)[0]);
+        }
+    },
+    startGame(gameChoice) {
+        switch (gameChoice) {
+            case 'connection-game':
+                this.startConnectGame();
+                break
+            default:
+        }
+    },
+    choiceHandler(event) {
+        event.preventDefault();
+        switch (event.target.getAttribute('class')) {
+            case 'game-choice':
+                app.startGame(event.target.getAttribute('id'));
+                break
+            case 'player-vote':
+                app.takeVote(event.target.getAttribute('id'));
+                break;
+            default:
+        }
+    }
 }
 
 app.nameForm.addEventListener('submit', e => {
     e.preventDefault();
     app.setPlayerName();
-})
-
-app.playArea.addEventListener('click', e => {
-    switch (e.target.getAttribute('id')) {
-        case 'connection-game':
-            app.startConnectGame();
-            break
-        default:
-    }
 })
